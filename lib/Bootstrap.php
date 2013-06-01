0ee085cf9e238dbf8b186335dc5f4cef5d193ad9
Graham Newton <grahamn95@outlook.com>
2013-06-01 12:10:57 +0100

    Version Compare and killing Magic quotes

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -3,9 +3,17 @@
 	//Require the core files
 	require LIB.'Basics.php';
 	require LIB.'Classes'.DS.'Ion.php';
+
+    //Check PHP version before we continue
+    if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+        die("Outdated PHP Version, you are running PHP Version: ".PHP_VERSION.". You need to be running 5.4 or greater.");
+    }
+
+    //Kill magic quotes
+    @magic_quotes_runtime(0);
 	
 	//set the autoload register to Ion::_load
-	spl_autoload_register(array('Ion', '_load'));
+	spl_autoload_register(['Ion', '_load']);
 	
 	//Load essential class files
 	Ion::load('Network', 'Classes');
