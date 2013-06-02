f168bdaf8f6aa10841ee5a3ced43dda58ab50dd7
Lewis Scally <lewisscally@gmail.com>
2013-06-02 11:53:58 +0100

    Removed lines 12 and 13 from bootstrap.

--- lib/Bootstrap.php
+++ lib/Bootstrap.php
@@ -8,9 +8,6 @@
     if (version_compare(PHP_VERSION, '5.4.0', '<')) {
         die("Outdated PHP Version, you are running PHP Version: ".PHP_VERSION.". You need to be running 5.4 or greater.");
     }
-
-    //Kill magic quotes
-    @magic_quotes_runtime(0);
 	
 	//set the autoload register to Ion::_load
 	spl_autoload_register(['Autoloader', '_load']);
